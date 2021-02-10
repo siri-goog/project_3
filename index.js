0fa714a17ad59f7945d6ed561b93e258500414e5
Siripond <goog.mullanu@gmail.com>
2021-02-10 22:29:43 +1100

    updated comments

--- index.js
+++ index.js
@@ -4,20 +4,27 @@ const app = express()
 const PORT = 3000
 const db = require('./db/data.js')
 
-//root path
+//Step 2 : Create the first routes to return all the information
+//--root path
 app.get('/', (req, res) => {
     res.send('Welcome to our schedule website')
 })
-//users details
+//--users path
 app.get('/users', (req, res) => {
     res.json(db.users)
 })
-//user details (specific index)
+//--schedules path
+app.get('/schedules', (req, res) => {
+    res.json(db.schedules)
+})
+
+//Step 3 : Create parameterized routes
+//--user details (specific index)
 app.get('/users/:usersParams', (req, res) => {
     //console.log(req.params.usersParams)
     res.send(db.users[req.params.usersParams])
 })
-//user details (specific index) and schedule details
+//--user details (specific index) and schedule details
 app.get('/users/:usersParams/schedules', (req, res) => {
     var obj = db.schedules
     var result = []
@@ -29,10 +36,26 @@ app.get('/users/:usersParams/schedules', (req, res) => {
     }
     res.send(result)
 })
-//schedules path
-app.get('/schedules', (req, res) => {
-    res.json(db.schedules)
-})
+
+//Step 4 : Create routes to update data
+    //example
+    //queries
+    //? = begining of query
+    //key:value separated by the ""="" symbol
+    //key:value pairs separated by the "&" symbol
+    //ex. /hello?name=GooG&car=Toyota
+    app.get('/hello', (req, res) => {
+        console.log(req.query.name)
+        if (req.query.name) {
+            res.send(`hello back at you ${req.query.name}!`)
+        } else {
+            res.send('hello nameless')
+        }
+        
+    })
+//--add a new schedule
+
+//--add a new user
 
 //set up server
 app.listen(PORT, () => {
